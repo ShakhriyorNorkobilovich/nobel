2170dc1027a82227c031d2fadb2bf53b11a94ae5
ShakhriyorNorkobilovich <shaxroyorbek2101@gmail.com>
2024-03-19 23:50:32 +0900

    feat: project started with express N1

---
+++ server.js
@@ -0,0 +1,27 @@
+const http = require("http");
+// const fs = require("fs");
+// const app = require("./app");
+const mongodb = require("mongodb");
+
+
+let db;
+const connectionString = "mongodb+srv://shaxroyorbek2101:Z@rifa3112@cluster0.rs251bp.mongodb.net";
+mongodb.connect(connectionString, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+}, (err, client) => {
+    if(err) console.log("ERROR on connection MongoDB");
+    else{
+        console.log("MongoDB connection succeed");
+        module.exports = client;
+        //console.log(client);
+        const app = require("./app");
+        const server = http.createServer(app);
+        let PORT = 3007;
+        server.listen(PORT, function () {
+        console.log(`The server is running succesfully on port: ${PORT}, http://localhost:${PORT}`);
+});
+    }
+});
+
+
